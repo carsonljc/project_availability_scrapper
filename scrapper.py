e174952a372a7173f930227758e790ae32171a26
Carson Liu <carsonliu@MacBook-Pro.local>
2020-06-18 19:48:06 -0700

    Integrated email sending

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,18 @@
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
+
 import pandas as pd
 import json
 import os
+
+RECIPIENTS = ["email@domain.com"]
+ROOTPATH = "."
+FROMEMAIL = "email@domain.com"
+FROMPASS = "password"
 
 class ProjectReader(object):
     def __init__(self, rootpath):
@@ -17,7 +29,7 @@ class ProjectReader(object):
                 self.old_projects = json.loads(json_file.read())
         except OSError:
             self.old_projects = []
-            print("No previous cache exists. Assuming no previous projects.")
+            print("INFO: No previous cache exists. Assuming no previous projects.")
 
     def get_name(self, item):
         name = str(item["ID"]) + str(item["Supervisor"]) + str(item["Project Title"])
@@ -25,14 +37,40 @@ class ProjectReader(object):
 
     def __write_cache(self, filename):
         cache_list = []
-        for index, row in self.projects.iterrows():
+        for _, row in self.projects.iterrows():
             name = self.get_name(row)
             cache_list.append(name)
         with open(filename, 'w') as outfile:
             json.dump(cache_list, outfile)
 
     def send_email(self, new_projects):
-        print("Not implemented")
+        if new_projects.empty:
+            print("INFO: No new projects, skipping sending email.")
+            return
+
+        print(new_projects)
+
+        msg = MIMEMultipart()
+        msg['Subject'] = "New ECE496 Projects Posted!"
+        msg['From'] = FROMEMAIL
+
+
+        html = """\
+        <html>
+        <head></head>
+        <body>
+            {0}
+        </body>
+        </html>
+        """.format(new_projects.to_html())
+
+        part1 = MIMEText(html, 'html')
+        msg.attach(part1)
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(FROMEMAIL, FROMPASS)
+        server.sendmail(msg['From'], RECIPIENTS , msg.as_string())
 
     def check_for_new(self):
         new_projects = []
@@ -43,10 +81,8 @@ class ProjectReader(object):
                 continue
             else:
                 new_projects += [index]
-        print(self.projects.loc[new_projects, :])
-        self.send_email(new_projects)
+        self.send_email(self.projects.loc[new_projects, :])
         self.__write_cache(os.path.join(self.rootpath, "ece496fetcher.json"))
 
-
-project_reader = ProjectReader(".")
+project_reader = ProjectReader(ROOTPATH)
 project_reader.check_for_new()
